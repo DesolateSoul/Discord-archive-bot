b4c70c4774be6055237705ea257fb2e203116e18
DesolateSoul <con1337@yandex.ru>
2023-06-06 08:15:40 +0700

    changed token

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 settings = {
-    'token': 'ODIzODg0NDIyNDQwNTUwNDkw.GG_ngT.g62q-JiGmTWDi4MzdKjvu8h9cLPooVOeesHa_k',
+    'token': '',
     'bot': 'archive-bot',
     'id': 823884422440550490,
     'prefix': '/'
